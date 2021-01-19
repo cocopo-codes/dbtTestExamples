46fd7d24f674869b1d6e20d44848e7f18bc50371
corissawex <corissa.haury@wexinc.com>
2021-01-18 19:51:16 -0500

    Update statistics.sql

--- models/3-game_stats/statistics.sql
+++ models/3-game_stats/statistics.sql
@@ -5,28 +5,31 @@
 }}
 
 with game_log as (
-	select 
-		distinct username,
+    select
+		distinct username as username,
 		sum(kills) as total_kills,
 		sum(deaths) as total_deaths,
 		sum(damage_done) as total_damage_done
-	from {{ref('game_log_vw')}}
-	group by username
+	from {{ ref('game_log_vw') }} t2
+	where t2.username is not null
+	group by t2.username
 ),
 
-user_info as (
-    select
-        username,
-        date_joined
-    from {{ref('user_data_vw')}}
+users_info as (
+ select username, date_joined, email
+ from {{ ref('user_data_vw') }}
+ group by username, date_joined, email
 ),
 
 join_all as (
-    select *
-    from user_info t1
-    full outer join game_log t2
-        on t1.username
-    order by t2.username
+    select t1.username gamertag,
+           t1.total_kills,
+           t1.total_deaths,
+           t1.total_damage_done,
+           t2.date_joined,
+           t2.email
+    from game_log t1
+    left join users_info t2 on t2.username = t1.username
 )
 
 select * from join_all
