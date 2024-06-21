bf40d7a8dfa85014fdb65879dba26fe49be2a407
Coco Poley <coco_writes@icloud.com>
2024-06-21 15:58:25 -0400

    fixing data tests

--- tests/users/tablePresent-users.sql
+++ tests/users/tablePresent-users.sql
@@ -1,5 +1,5 @@
 -- A quick test to check that the table is present
 select count(*) as row_count
 	from {{ref('user_data_vw')}}
-	where username is not null
+	where USERNAME is not null
 	having row_count <= 0
